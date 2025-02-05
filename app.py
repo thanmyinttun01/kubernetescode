45896c6dca8aa912497795688cc247155c178e19
Than Myint Tun <146816681+thanmyinttun01@users.noreply.github.com>
2025-02-05 13:54:17 +0630

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Ko than myint!! , Welcome to our Channel Web Site!'
+    return 'Hello Ko than myint tun, hope you are doing well and welcome to our Channel Web Site!'
